055cc60e337b5e8c3de2070a59b0001b550fefcb
IndimaDias <indimad@yahoo.com>
2019-08-11 21:11:02 -0500

    Updates

---
+++ precache-manifest.31c33c4734043994ba843ac32b2ecb62.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5396794e3ce14fdc5e64dc7228165dc5",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "8b2f95717b1d58e9f3db",
+    "url": "/memory-game/static/css/main.6b695278.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/memory-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "8b2f95717b1d58e9f3db",
+    "url": "/memory-game/static/js/main.6540ad8a.chunk.js"
+  },
+  {
+    "revision": "799512f57ffe7b17c145",
+    "url": "/memory-game/static/js/runtime~main.4bfb86a8.js"
+  }
+]);
